576e012f7c51696a533ba263e2634f8c4f2e6285
Osei <williamsabodunrin@gmail.com>
2023-12-07 15:24:51 +0000

    chess engine fix

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 engine= ""
 
 engine_dict = {
-    'stockfish' : "stockfish-windows-x86-64-avx2/stockfish/stockfish-windows-x86-64-avx2.exe",
-    'komodo' : "komodo-14/komodo-14_224afb/Windows/komodo-14.1-64bit-bmi2.exe"
+    'stockfish' : "stockfish-windows-x86-64-avx2/stockfish-windows-x86-64-avx2.exe",
+    'komodo' : "komodo-14/komodo-14.1-64bit-bmi2.exe"
 }
 
 @app.route('/', strict_slashes=False)
